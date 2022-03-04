80d1eb699e47fba64e4cefdd952e145722d5f444
fegusta <fegtnc@gmail.com>
2022-03-04 20:23:49 -0300

    Criação CadastroJogoActivity.k

---
+++ app/src/main/java/com/fegusta/fegustarecyclerview/ui/CadastroJogoActivity.kt
@@ -0,0 +1,12 @@
+package com.fegusta.fegustarecyclerview.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fegusta.fegustarecyclerview.R
+
+class CadastroJogoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_jogo)
+    }
+}
--- app/src/main/java/com/fegusta/fegustarecyclerview/ui/MainActivity.kt
+++ app/src/main/java/com/fegusta/fegustarecyclerview/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.fegusta.fegustarecyclerview
+package com.fegusta.fegustarecyclerview.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.fegusta.fegustarecyclerview.R
 import com.fegusta.fegustarecyclerview.adapter.JogosAdapter
 import com.fegusta.fegustarecyclerview.datasource.DataSource
 
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun iniciarRecyclerView() {
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewJogos)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
         recyclerView.adapter = JogosAdapter(DataSource.getJogos())
     }
 }

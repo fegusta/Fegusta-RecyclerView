4fe8cfe0633289427f3986fce3a8782c66c846ed
fegusta <fegtnc@gmail.com>
2022-03-05 11:40:02 -0300

    Add FloatingAction na MainActivity Nagevaga para Cadatro Jogo

--- app/src/main/java/com/fegusta/fegustarecyclerview/ui/MainActivity.kt
+++ app/src/main/java/com/fegusta/fegustarecyclerview/ui/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.fegusta.fegustarecyclerview.ui
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.controls.actions.FloatAction
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fegusta.fegustarecyclerview.R
 import com.fegusta.fegustarecyclerview.adapter.JogosAdapter
 import com.fegusta.fegustarecyclerview.datasource.DataSource
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         iniciarRecyclerView()
+
+        var buttomCadastrarJogo =findViewById<FloatingActionButton>(R.id.butotonCadastrarJogo)
+        buttomCadastrarJogo.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if(v!!.id == R.id.butotonCadastrarJogo){
+            val intent = Intent(this, CadastroJogoActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun iniciarRecyclerView() {
@@ -21,4 +36,5 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
         recyclerView.adapter = JogosAdapter(DataSource.getJogos())
     }
+
 }

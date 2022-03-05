a843280b0ae600a9a728aef78e1ce1374336ccd2
fegusta <fegtnc@gmail.com>
2022-03-05 20:10:26 -0300

    ações itens de menu

--- app/src/main/java/com/fegusta/fegustarecyclerview/ui/CadastroJogoActivity.kt
+++ app/src/main/java/com/fegusta/fegustarecyclerview/ui/CadastroJogoActivity.kt
@@ -2,6 +2,9 @@ package com.fegusta.fegustarecyclerview.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.fegusta.fegustarecyclerview.R
 
 class CadastroJogoActivity : AppCompatActivity() {
@@ -15,5 +18,26 @@ class CadastroJogoActivity : AppCompatActivity() {
     private fun insertToolbar() {
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar!!.title = "Novo Jogo"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_game, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_cancelar -> {
+                Toast.makeText(this,"Cancelar",Toast.LENGTH_SHORT).show()
+            }
+            R.id.menu_salvar -> {
+                Toast.makeText(this,"Salvar",Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

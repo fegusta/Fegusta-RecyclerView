5739038fa7d1c3e02b6aa29b0dbf7b18814060f4
fegusta <fegtnc@gmail.com>
2022-03-06 11:03:36 -0300

    comando para criação da tabela

--- app/src/main/java/com/fegusta/fegustarecyclerview/datasource/DatabaseHelper.kt
+++ app/src/main/java/com/fegusta/fegustarecyclerview/datasource/DatabaseHelper.kt
@@ -4,17 +4,25 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,null, DATABASE_NAME) {
-    override fun onCreate(p0: SQLiteDatabase?) {
+class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
         TODO("Not yet implemented")
     }
 
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
     }
 
     companion object {
         private const val DATABASE_NAME = "jogo.db"
         private const val DATABASE_VERSION = 1
+
+        private const val  CREATE_TABLE_JOGO = "CREATE TABLE ${DatabaseDefinitions.Jogo.TABLE_NAME} (" +
+                "${DatabaseDefinitions.Jogo.Columns.ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "${DatabaseDefinitions.Jogo.Columns.TITULO} TEXT, " +
+                "${DatabaseDefinitions.Jogo.Columns.PRODUTORA} TEXT, " +
+                "${DatabaseDefinitions.Jogo.Columns.NOTA} REAL, " +
+                "${DatabaseDefinitions.Jogo.Columns.CONSOLE} TEXT, " +
+                "${DatabaseDefinitions.Jogo.Columns.ZERADO} INTEGER);"
     }
 }
